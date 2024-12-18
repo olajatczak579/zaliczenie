20eef5c54014fc2cade64cb26108f8a0a6b2cbdd
Aleksandra Jatczak <255615@edu.p.lodz.pl>
2024-12-18 15:04:25 +0100

    01

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
+int policz(int a, int b) {
+    return a + b;
+}
+
 int main(void) {
     printf("Hello, World!\n");
+    printf("W pierwszym branchu: 2 + 3 = %d\n", policz(2, 3));
     return 0;
 }
